29bf1727ddc2faf502538c4feeb3ea8ba4680925
EastZero123 <cdy1005p@naver.com>
2022-09-17 00:28:50 +0900

    Next.js CH02 finish

---
+++ udemy/nextjs-course-code/pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,9 @@
+function SelectedClientProjectPage() {
+  return (
+    <div>
+      <h1>The Project Page ...</h1>
+    </div>
+  );
+}
+
+export default SelectedClientProjectPage;
---
+++ udemy/nextjs-course-code/pages/clients/[id]/index.js
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router";
+
+function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push("/clients/max/projecta");
+  }
+
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
+}
+
+export default ClientProjectsPage;
