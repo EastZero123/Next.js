8413199a4a8ab2342d8f0148f17cb44010f0fa4b
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2022-09-30 15:34:48 +0900

    CH14-1 finish

---
+++ udemy/nextjs-course-code-CH14/pages/news/[newsId].js
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router"
+
+function DetailPage() {
+  const router = useRouter()
+}
+
+export default DetailPage
---
+++ udemy/nextjs-course-code-CH14/pages/news/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react"
+import Link from "next/link"
+
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">Next JS</Link>
+        </li>
+      </ul>
+    </Fragment>
+  )
+}
+
+export default NewsPage
